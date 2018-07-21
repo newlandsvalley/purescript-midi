cc11c15321fe3763d7b4c10b6a1b58b1da0f82f9
John Watson <john.watson@gmx.co.uk>
2018-07-21 15:24:15 +0100

    Migrate keyboard-signal example to PS 12
    and purescript-midi v2.0.0

--- examples/keyboard-signal/README.md
+++ examples/keyboard-signal/README.md
@@ -1,11 +1,11 @@
 Web-Midi example - Keyboard-Signal
 ==================================
 
-purescript-midi version 1.1.0
+purescript-midi version 2.0.0 (PureScript 12.0)
 
 This is a basic example that shows how you can detect MIDI input devices such as keyboards when they
 connect or disconnect and then detect MIDI messages when (for example) keys are pressed.
-You will need a MIDI keyboard or other such device to try it. 
+You will need a MIDI keyboard or other such device to try it.
 
 You must first connect to Web-Midi - then you subscribe to signals of input device connection/disconnection messages and to Midi Event messages that occur when keys are pressed.  These events are then logged to the console.
 
--- examples/keyboard-signal/src/Main.purs
+++ examples/keyboard-signal/src/Main.purs
@@ -1,17 +1,17 @@
 module Main where
 
 import Prelude (Unit, bind, (<>))
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Effect (Effect)
+import Effect.Console (log, logShow)
 
 import Data.Midi.WebMidi
 import Data.Maybe (Maybe(..))
 import Data.Midi (TimedEvent(..)) as Midi
 
-import Signal.Channel (CHANNEL, subscribe)
+import Signal.Channel (subscribe)
 import Signal (Signal, runSignal, (~>))
 
-logDevice :: ∀ eff. Device -> Eff (console :: CONSOLE | eff) Unit
+logDevice :: Device -> Effect Unit
 logDevice d =
   let
     connectState =
@@ -22,20 +22,20 @@ logDevice d =
   in
     log ("MIDI device: " <> d.id <> connectState)
 
-logMidiEvent :: ∀ eff. Midi.TimedEvent -> Eff (console :: CONSOLE | eff) Unit
+logMidiEvent :: Midi.TimedEvent -> Effect Unit
 logMidiEvent (Midi.TimedEvent e) =
   case e.event of
     Just mev -> logShow mev
     Nothing -> log "event parse error "
 
-logEventEffect :: forall eff. Signal Midi.TimedEvent -> Signal (Eff (console :: CONSOLE | eff) Unit)
+logEventEffect :: Signal Midi.TimedEvent -> Signal (Effect Unit)
 logEventEffect s = s ~> logMidiEvent
 
-logDeviceEffect :: forall eff. Signal Device -> Signal (Eff (console :: CONSOLE | eff) Unit)
+logDeviceEffect :: Signal Device -> Signal (Effect Unit)
 logDeviceEffect s = s ~> logDevice
 
 -- | test web-midi event recognition
-testEvents :: forall eff. Eff (wm :: WEBMIDI, console :: CONSOLE, channel :: CHANNEL | eff) Unit
+testEvents :: Effect Unit
 testEvents = do
   -- create a channel for MIDI event messages and start
   -- feeding it from web-midi events (first message is a dummy)
@@ -47,7 +47,7 @@ testEvents = do
   runSignal (logEventEffect eventSignal)
 
 -- | test web-midi device connectipon disconnection
-testDevices :: forall eff. Eff (wm :: WEBMIDI, console :: CONSOLE, channel :: CHANNEL | eff) Unit
+testDevices :: Effect Unit
 testDevices = do
   -- create a channel for MIDI device connections and disconnections and start
   dChannel <- createDeviceChannel
@@ -57,7 +57,7 @@ testDevices = do
   -- and finally log the messages
   runSignal (logDeviceEffect deviceSignal)
 
-main :: forall eff. Eff (wm :: WEBMIDI, console :: CONSOLE, channel :: CHANNEL | eff) Unit
+main :: Effect Unit
 main = do
   -- connect to web-midi
   _ <- webMidiConnect
